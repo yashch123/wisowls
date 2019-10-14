76b7159d1fc8dd320453f4b8bdbae5f94aca5bee
Shab <sh1994b@g.ucla.edu>
2019-10-14 22:57:21 +0000

    added login page and hooked to backend - error message given when credentials are incorrect. must fix flash msg issue so that it will be visible

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'pages/about'
   get 'pages/home'
   get 'pages/login'
   get 'pages/profile'
   get  '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
