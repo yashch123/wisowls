c214b9cbeb0af176db4fe0439a84a33663461d5c
Will Htun <willhtun42@gmail.com>
2019-10-15 06:01:20 +0000

    Persistent session and cookies. Rerouting if already logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,18 @@
 class SessionsController < ApplicationController
   def new
+    # If user is logged in, redirect to user page
+    if current_user
+      log_in current_user
+      remember current_user
+      redirect_to current_user
+    end
   end
-  
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -15,5 +21,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to login_url
   end
 end
