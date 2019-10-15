9b2865ec2df474480f64992dd6434f0198ab113b
Shab <sh1994b@g.ucla.edu>
2019-10-15 01:07:18 +0000

    implemented login, nothing in frontend yet (ch. 8.2.3 tutorial needs to be done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,14 @@ class SessionsController < ApplicationController
   def new
   end
   
+  
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user
     else
-      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   get 'sessions/new'
   get 'pages/about'
   get 'pages/home'
