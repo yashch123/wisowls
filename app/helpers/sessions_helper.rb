4521ffb71de1956d4ba9d86c2264168b57ebbc1e
Shab <sh1994b@g.ucla.edu>
2019-10-20 21:45:13 +0000

    fixed logging in issue, created dropdown menu

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,17 +11,11 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
-  
-  # Returns the user corresponding to the remember token cookie.
+
+  # Returns the current logged-in user (if any).
   def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 
