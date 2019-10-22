baf22383fd6b2b481105a5840d2301ed30c3fd4a
Shab <sh1994b@g.ucla.edu>
2019-10-22 05:43:47 +0000

    added edit profile feature with integration tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,35 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome!"
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
